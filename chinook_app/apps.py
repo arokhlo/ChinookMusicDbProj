4d30623579262c2d35c936f59fab4017ba0f0d96
Mortaza Zolfpour <mortazazolfpour@gmail.com>
2026-02-06 14:04:23 +0000

    update apps.py and requirements.txt

--- chinook_app/apps.py
+++ chinook_app/apps.py
@@ -1,5 +1,14 @@
-from django.apps import AppConfig 
- 
-class ChinookAppConfig(AppConfig): 
-    default_auto_field = 'django.db.models.BigAutoField' 
-    name = 'chinook_app' 
+from django.apps import AppConfig
+
+
+class ChinookAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chinook_app'
+    
+    def ready(self):
+        """Initialize error handling when app is ready."""
+        try:
+            # Import error handlers
+            from . import error_handlers
+        except ImportError:
+            pass
